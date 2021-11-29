d6f267b3f33ce1ddb55527798dc74f9e46347f74
biasg <georgebias@outlook.com.br>
2021-11-28 23:45:55 -0300

    :tada: Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a2fa2041369191dd25ed4d82aa508703'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dcc29c60-b1a6-403f-a9ef-93e2e94a0579'
+};
